92a6dc8ec3ef38f34ed35ee077d8ba78980c1cf6
jbaade <baade.c.j@gmail.com>
2022-05-04 19:44:18 -0700

    Removed unused import

--- src/View/MineSweeper.java
+++ src/View/MineSweeper.java
@@ -7,7 +7,6 @@ import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
